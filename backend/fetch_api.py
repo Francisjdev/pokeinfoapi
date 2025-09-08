a8901bfd15ef5120b4ee19a596720ecdb067a4b1
Francis <franciscorojasdev@proton.me>
2025-09-08 16:35:50 -0300

    updated db

--- backend/fetch_api.py
+++ backend/fetch_api.py
@@ -3,15 +3,15 @@ from datetime import date, timedelta
 
 def get_events():
     today = date.today()
-    end_date = today + timedelta(days=14)
+    end_date = today + timedelta(days=21)
 
     country = '_country/cl'
-    region = '_state/Santiago'
+    region = '_state/Santiago Metropolita'
     radius = '_radius/50'
     unit = '_unit/km'
     date_param = f'_start/{today}'
     end_param = f'_end/{end_date}'
 
-    url = f'https://pokedata.ovh/events/api/_tcg/cups/challenges/{country}/{region}/{radius}/{unit}/{date_param}/{end_param}'
+    url = f'https://pokedata.ovh/events/api/_tcg/cups/challenges/{country}/{radius}/{unit}/{date_param}/{end_param}'
     r = requests.get(url)
     return r.json()
